8a6431a2e921f4d5048cab83d19494e53b5eb02f
Romina Delgado <rominadelgado_@hotmail.com>
2017-11-27 22:57:36 -0300

    LIMPIANDO PRINTLN

--- PROYECTO/src/premio/PremioCampoProtector.java
+++ PROYECTO/src/premio/PremioCampoProtector.java
@@ -20,7 +20,6 @@ public class PremioCampoProtector implements Temporal	{
 	
 	public PremioCampoProtector(Defensa d, Juego j) {
 
-		System.out.println("voy a colocar un campo protector");
 		juego=j;
 		campo = new CampoProtector(this, d);
 		CeldaCompuesta celda = d.getCelda();
@@ -32,7 +31,7 @@ public class PremioCampoProtector implements Temporal	{
 			campo.setCelda(celda);
 			personaje.getGrafico().getLabel().setBorder(new MatteBorder(2, 2, 2, 2, Color.BLUE));
 			juego.reemplazarDefensa(campo, x, y);
-			System.out.println("cambie la defensa por el campo");
+
 			new Timer(this, 5000);
 		}
 	}
@@ -45,9 +44,7 @@ public class PremioCampoProtector implements Temporal	{
 		CeldaCompuesta celda = campo.getCelda();
 		personaje.getGrafico().getLabel().setBorder(null);
 		celda.destruirDefensa();
-		System.out.println("cambie el campo por la defensa");
 		juego.reemplazarDefensa(personaje, celda.getX(), celda.getY());
-		System.out.println("voy a eliminar el escudito");
 
 	}
 
